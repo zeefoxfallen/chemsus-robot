855b8cb646ddaec3d3c1318afb5f243b36a17ceb
zeefoxfallen <83886987+zeefoxfallen@users.noreply.github.com>
2021-07-31 17:49:00 -0600

    e
    e

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
-
+import json
+from discord.ext import commands
 client = discord.Client()
 
 @client.event
@@ -13,5 +14,29 @@ async def on_message(message):
 
     if message.content.startswith('+hello'):
         await message.channel.send('Hello!')
+
+@client.command()
+async def reactrole(ctx,  emoji, role: discord.role,*,message):
+
+    emb = discord.embed(description=message)
+    msg = await ctx.channel.send(embed=emb)
+    await msg.add_reaction(emoji)
+
+    with open('reactrole.json') as json_file:
+        data = json.load(json_file)
+
+        new_react_role = {
+            'role_name' :role.__name,
+            'role_id' :role.id,
+            'emoji' :emoji,
+            'message_id' :msg.id
+        }
+
+
+        data.append(new_react_role)
+
+        with open('reactrole.json','w') as j:
+            json.dump(data,j,indent=4)
+
 
 client.run("ODcxMTU3MTA0NjM4MDY2NzUx.YQXORw.u7owINfM7P7mlm9O29rWOW9GvCc")
