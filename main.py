8e5d557a76777f739e4a3b3cb8e0a4d8e415b84b
Ex-32 <77823377+Ex-32@users.noreply.github.com>
2021-07-31 21:32:12 -0500

    added $echo command, and commented out currently broken $reactrole

--- main.py
+++ main.py
@@ -21,27 +21,38 @@ async def on_message(message):
     if message.content.startswith('+hello'):
         await message.channel.send('Hello!')
 
+# # Temporaily Disabled Until Functional:
+# @bot.command()
+# async def reactrole(ctx,  emoji, role: discord.role,*,message):
+
+#     emb = discord.Embed(description=message)
+#     msg = await ctx.channel.send(embed=emb)
+#     await msg.add_reaction(emoji)
+
+#     with open('reactrole.json') as json_file:
+#         data = json.load(json_file)
+
+#         new_react_role = {
+#             'role_name' :role.__name,
+#             'role_id' :role.id,
+#             'emoji' :emoji,
+#             'message_id' :msg.id
+#         }
+
+#         data.append(new_react_role)
+
+#         with open('reactrole.json','w') as j:
+#             json.dump(data,j,indent=4)
+
 @bot.command()
-async def reactrole(ctx,  emoji, role: discord.role,*,message):
-
-    emb = discord.Embed(description=message)
-    msg = await ctx.channel.send(embed=emb)
-    await msg.add_reaction(emoji)
-
-    with open('reactrole.json') as json_file:
-        data = json.load(json_file)
-
-        new_react_role = {
-            'role_name' :role.__name,
-            'role_id' :role.id,
-            'emoji' :emoji,
-            'message_id' :msg.id
-        }
-
-
-        data.append(new_react_role)
-
-        with open('reactrole.json','w') as j:
-            json.dump(data,j,indent=4)
+async def echo(ctx, *args):
+    if args == ():
+        await ctx.channel.send(":zipper_mouth:")
+    else:
+        output = ""
+        for arg in args:
+            output += str(arg)
+            output += " "
+        await ctx.channel.send(output)
 
 bot.run("ODcxMTU3MTA0NjM4MDY2NzUx.YQXORw.u7owINfM7P7mlm9O29rWOW9GvCc")
