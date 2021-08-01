bc005bb8bf5acfd8ec43247f2247530e4929d8e5
Ex-32 <77823377+Ex-32@users.noreply.github.com>
2021-07-31 20:58:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import discord
 import json
 from discord.ext import commands
-client = discord.Client()
-client = commands.bot(command_prefix="+")
 
+bot = commands.Bot(command_prefix='$')
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    await bot.process_commands(message)
+
+    if message.author == bot.user:
         return
+
+    if message.content == "bot?":
+        await message.channel.send("Beep Boop")
 
     if message.content.startswith('+hello'):
         await message.channel.send('Hello!')
 
-@client.command()
+@bot.command()
 async def reactrole(ctx,  emoji, role: discord.role,*,message):
 
-    emb = discord.embed(description=message)
+    emb = discord.Embed(description=message)
     msg = await ctx.channel.send(embed=emb)
     await msg.add_reaction(emoji)
 
@@ -40,5 +44,4 @@ async def reactrole(ctx,  emoji, role: discord.role,*,message):
         with open('reactrole.json','w') as j:
             json.dump(data,j,indent=4)
 
-
-client.run("ODcxMTU3MTA0NjM4MDY2NzUx.YQXORw.u7owINfM7P7mlm9O29rWOW9GvCc")
+bot.run("ODcxMTU3MTA0NjM4MDY2NzUx.YQXORw.u7owINfM7P7mlm9O29rWOW9GvCc")
