e926a5cb173fd1c57dc8a13e640e66c7e1515470
Ex-32 <77823377+Ex-32@users.noreply.github.com>
2021-08-29 12:39:32 -0500

    fixed bash scripts

--- bin/build.sh
+++ bin/build.sh
@@ -4,10 +4,14 @@ pushd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null
 
 python3 -m venv --prompt "chemsus-robot .venv" ../.venv
 
-bash -c "source ../.venv/bin/activate ;\
-python3 -m pip install --upgrade pip ;\
-pip install -r ../requirements.txt ;\
-pip list ;\
-deactivate "
+source ../.venv/bin/activate 
+
+python3 -m pip install --upgrade pip 
+
+pip install -r ../requirements.txt 
+
+pip list 
+
+deactivate 
 
 popd > /dev/null
--- bin/run.sh
+++ bin/run.sh
@@ -2,10 +2,10 @@
 
 pushd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null
 
-python3 -m venv --prompt "chemsus-robot .venv" ../.venv
+source ../.venv/bin/activate 
 
-bash -c "source ../.venv/bin/activate ;\
-python3 ../bot/main.py ;\
-deactivate "
+python3 ../bot/main.py 
+
+deactivate 
 
 popd > /dev/null
