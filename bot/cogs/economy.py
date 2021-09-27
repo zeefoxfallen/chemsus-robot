1bde88209e712865dcf4c3a59a9fae0f41282ab5
zeefoxfallen <83886987+zeefoxfallen@users.noreply.github.com>
2021-09-26 21:02:09 -0600

    economy stuff

--- bot/cogs/economy.py
+++ bot/cogs/economy.py
@@ -2,16 +2,17 @@
 from discord.ext import commands
 
 #imports data needed for economy
-from economydata import accounts
+from economydata.accounts import accountsl
 
 class economy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
         #setup profile
-        @commands.command()
-        async def estart(ctx, member):
-            if accounts == member.id:
-                await ctx.channel.send("you already have a account")
-            else:
-                accounts.append(member.id)
+    @commands.command()
+    async def estart(self,ctx):
+        if ctx.author.id in accountsl:
+            await ctx.channel.send("you already have a account")
+        else:
+            accountsl.append(ctx.author.id)
+            await ctx.channel.send("your account has been made")
